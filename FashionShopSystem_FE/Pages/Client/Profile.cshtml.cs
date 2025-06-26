c96e24efc43364e50435214b3c8a75b41dbe63a0
MeabuHC <woolythesheepnew@gmail.com>
2025-06-26 22:11:41 +0700

    Login logout

---
+++ FashionShopSystem_FE/Pages/Client/Profile.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FashionShopSystem_FE.Pages.Client
+{
+    public class ProfileModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+} 
