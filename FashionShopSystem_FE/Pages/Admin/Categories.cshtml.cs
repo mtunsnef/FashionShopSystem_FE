0e458a35f9e20d9b4bba94cb8a8cda3f70bb8f52
MeabuHC <woolythesheepnew@gmail.com>
2025-07-22 23:15:55 +0700

    Done admin

---
+++ FashionShopSystem_FE/Pages/Admin/Categories.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FashionShopSystem_FE.Pages.Admin
+{
+    public class CategoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
---
+++ FashionShopSystem_FE/Pages/Admin/Orders.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FashionShopSystem_FE.Pages.Admin
+{
+    public class OrdersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
